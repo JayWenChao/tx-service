5b26915c455043f6305a57c3f0297d805e52511b
wenchao <1qazQAZ>
2020-05-09 19:13:39 +0800

    优化python

--- tx-service-python/txclassmate/txclassmate/main.py
+++ tx-service-python/txclassmate/txclassmate/main.py
@@ -5,3 +5,4 @@ if __name__ == "__main__":
     print("start executor model %s main..." %(sys.argv[1]))
     sys.path.append(os.path.dirname(os.path.abspath(__file__)))
     execute((['scrapy', 'crawl', sys.argv[1]]))
+
